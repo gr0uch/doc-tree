d9c366dd36e72c26304ff74aa10bc8f827333b3e
Dali Zheng <d@liwa.li>
2015-02-02 16:19:31 -0800

    split up match nodes function, change test reporter

--- test/run.js
+++ test/run.js
@@ -9,7 +9,7 @@ Test('Class documentation', t => {
 
   t.equal(output.length, 5, 'Docs match up to corresponding nodes.');
   t.equal(output[0].context.path, 'Person', 'Class path is correct.');
-  t.equal(output[0].comment.description.match(/\<p\>/g).length, 2,
+  t.equal(output[0].comment.description.match(/<p>/g).length, 2,
     'CommonMark is supported.');
   t.equal(output[0].context.type, 'class', 'Class is a class.');
   t.equal(output[2].context.kind, 'get', 'Getter is a getter.');
