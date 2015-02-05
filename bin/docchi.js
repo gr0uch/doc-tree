865563e40699aee5880d0eb2a49e9f76e22b6019
Dali Zheng <dali@series.ac>
2015-02-04 17:55:39 -0800

    move cli code to bin

--- bin/docchi.js
+++ bin/docchi.js
@@ -1,4 +1,29 @@
 #!/usr/bin/env node
 
-require('6to5/register');
-require('../lib/cli');
+var Docchi = require('../');
+var chunks = [];
+
+process.stdin.on('readable', function () {
+  var chunk = process.stdin.read();
+
+  if (!!chunk) {
+    chunks.push(chunk);
+  }
+});
+
+process.stdin.on('end', function () {
+  var inputBuffer = Buffer.concat(chunks);
+  var output;
+  var doc;
+
+  try {
+    doc = new Docchi(inputBuffer);
+    output = doc.output();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  process.stdout.write(JSON.stringify(output, null, 2));
+  process.exit(0);
+});
