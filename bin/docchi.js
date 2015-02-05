64a1edd2351db210a7468cb5a0253c560fbeb497
Dali Zheng <dali@series.ac>
2015-02-04 18:30:48 -0800

    fix CLI by including polyfill

--- bin/docchi.js
+++ bin/docchi.js
@@ -1,29 +1,4 @@
 #!/usr/bin/env node
 
-var Docchi = require('../');
-var chunks = [];
-
-process.stdin.on('readable', function () {
-  var chunk = process.stdin.read();
-
-  if (!!chunk) {
-    chunks.push(chunk);
-  }
-});
-
-process.stdin.on('end', function () {
-  var inputBuffer = Buffer.concat(chunks);
-  var output;
-  var doc;
-
-  try {
-    doc = new Docchi(inputBuffer);
-    output = doc.output();
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-
-  process.stdout.write(JSON.stringify(output, null, 2));
-  process.exit(0);
-});
+require("6to5/polyfill");
+require('../dist/cli');
