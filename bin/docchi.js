114806f5e6430e500a2132a77813e144e6a2a29e
Dali Zheng <dali@series.ac>
2015-02-04 16:56:24 -0800

    small refactor of CLI, minimal entry point

--- bin/docchi.js
+++ bin/docchi.js
@@ -1,29 +1,4 @@
 #!/usr/bin/env node
 
-var Docchi = require('../');
-var chunks = [];
-var doc;
-
-process.stdin.on('readable', function () {
-  var chunk = process.stdin.read();
-
-  if (!!chunk) {
-    chunks.push(chunk);
-  }
-});
-
-process.stdin.on('end', function () {
-  var inputBuffer = Buffer.concat(chunks);
-  var output;
-
-  try {
-    doc = new Docchi(inputBuffer);
-    output = doc.output();
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-
-  process.stdout.write(JSON.stringify(output, null, 2));
-  process.exit(0);
-});
+require('6to5/register');
+require('../lib/cli');
