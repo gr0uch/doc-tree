41b5bef708aaf25992342ca93fbfe30b8b3877c4
EarthyOrange <nikhilsinghal90@gmail.com>
2021-08-16 11:46:11 -0700

    feat: generate data for spread operator (#25)
    Co-authored-by: Nikhil Singhal <niksinghal@paypal.com>

--- test/variable.js
+++ test/variable.js
@@ -9,8 +9,11 @@ test('Variable assignment', t => {
   const doc = docTree.parse(readFile('fixtures/fixture3.js'))
   const output = doc.output()
 
-  t.equal(output.filter(node => node.context).length,
-    8, 'docs match up to corresponding nodes')
+  t.equal(
+    output.filter(node => node.context).length,
+    11,
+    'docs match up to corresponding nodes'
+  )
 
   t.equal(output[0].context.name, 'anonFn', 'name is correct')
   t.equal(output[0].context.type, 'variable', 'type is correct')
@@ -39,6 +42,18 @@ test('Variable assignment', t => {
   t.equal(output[7].context.name, 'bar', 'name is correct')
   t.equal(output[7].context.type, 'function', 'type is correct')
   t.equal(output[7].context.generator, true, 'generator is correct')
+
+  t.equal(output[8].context.name, 'spreadObj', 'name is correct')
+  t.equal(output[8].context.type, 'variable', 'type is correct')
+
+  t.equal(output[9].context.name, 'obj', 'name is correct')
+  t.equal(output[9].context.type, 'property', 'type is correct')
+  t.equal(output[9].context.target, 'spreadObj', 'target is correct')
+  t.ok(output[9].context.spreadProperty, 'Is Spread Property')
+
+  t.equal(output[10].context.name, 'prop1', 'name is correct')
+  t.equal(output[10].context.type, 'property', 'type is correct')
+  t.equal(output[10].context.target, 'spreadObj', 'target is correct')
 
   t.end()
 })
